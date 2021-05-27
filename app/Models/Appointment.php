1fcc52a86b3dc06b2ea9c8d189f361de6bde3431
clovon <channel.clovon@gmail.com>
2021-05-28 03:31:20 +0545

    fix styling

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Appointment extends Model
 {

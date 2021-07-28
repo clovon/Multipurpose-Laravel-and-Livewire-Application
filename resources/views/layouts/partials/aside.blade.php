c6a7674def7995254ed2438edb59478e3ba01991
clovon <channel.clovon@gmail.com>
2021-07-28 22:17:09 +0545

    clean up

--- resources/views/layouts/partials/aside.blade.php
+++ resources/views/layouts/partials/aside.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="index3.html" class="brand-link">
-    <img src="{{ asset('backend/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+    <img src="https://yt3.ggpht.com/yti/APfAmoGttBvvoOAMsBnAXftYUi5cJIEFmqfHEjEs8zh7=s88-c-k-c0x00ffffff-no-rj-mo" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
          style="opacity: .8">
     <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
   </a>
